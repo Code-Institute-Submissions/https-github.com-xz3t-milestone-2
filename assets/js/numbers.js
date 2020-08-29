33133b7b04fd56e7af50717a3c3358794dfa8df6
xz3t <leu.valentin@gmail.com>
2020-08-29 13:45:27 +0000

    fixed: css - responsiveness for small screen, js- data-turnable and win screen issue, index.html - small visual fixes

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -12,31 +12,43 @@ shuffle(numbers);
 distributeNumbers();
 
 for (i = 0; i < buttons.length; i++) {
-  buttons[i].setAttribute('onclick', getSound(buttons[i].dataset.number)) //getgsound(0)
-  buttons[i].addEventListener('click', function (e) {
+  // buttons[i].setAttribute("onclick", getSound(buttons[i].dataset.number)); moved to distributeNumbers function
+  buttons[i].addEventListener("click", function (e) {
     var turnable = e.target.dataset.turnable;
- 
+
     //action for first click
-    if (!wait && lastKnownButtonId == undefined && lastKnownButtonNumber == undefined && turnable == 'true') {
-      e.target.dataset.turnable = 'false';
+    if (
+      !wait &&
+      lastKnownButtonId == undefined &&
+      lastKnownButtonNumber == undefined &&
+      turnable == "true"
+    ) {
+      e.target.dataset.turnable = "false";
 
       e.target.innerHTML = getgImage(event.target.dataset.number);
-      e.target.style.backgroundColor = 'orange';
+      e.target.style.backgroundColor = "orange";
 
       lastKnownButtonId = e.target.id;
       lastKnownButtonNumber = e.target.dataset.number;
     }
 
     //action for second click
-    else if (!wait && lastKnownButtonId != undefined && lastKnownButtonNumber != undefined && turnable == 'true' && e.target.id != lastKnownButtonId) {
-      e.target.dataset.turnable = 'false';
+    else if (
+      !wait &&
+      lastKnownButtonId != undefined &&
+      lastKnownButtonNumber != undefined &&
+      turnable == "true" &&
+      e.target.id != lastKnownButtonId
+    ) {
+      e.target.dataset.turnable = "false";
 
       e.target.innerHTML = getgImage(event.target.dataset.number);
 
       //if its a match
       if (e.target.dataset.number == lastKnownButtonNumber) {
-        e.target.style.backgroundColor = 'green';
-        document.getElementById(lastKnownButtonId).style.backgroundColor = 'green';
+        e.target.style.backgroundColor = "green";
+        document.getElementById(lastKnownButtonId).style.backgroundColor =
+          "green";
 
         lastKnownButtonId = undefined;
         lastKnownButtonNumber = undefined;
@@ -46,42 +58,39 @@ for (i = 0; i < buttons.length; i++) {
         if (matches == 8) {
           showWinScreen();
         }
-
       }
 
       //if its not a match
       else {
-        document.getElementById(lastKnownButtonId).style.backgroundColor = 'red';
-        e.target.style.backgroundColor = 'red';
+        document.getElementById(lastKnownButtonId).style.backgroundColor =
+          "red";
+        e.target.style.backgroundColor = "red";
         wait = true;
 
         setTimeout(() => {
-          e.target.dataset.turnable = 'true';
-          e.target.style.backgroundColor = 'white'
+          e.target.dataset.turnable = "true";
+          e.target.style.backgroundColor = "white";
           e.target.innerHTML = getgImage(0);
 
+          var tempLastClickedButton = document.getElementById(
+            lastKnownButtonId
+          );
 
-          var tempLastClickedButton = document.getElementById(lastKnownButtonId);
-
-          tempLastClickedButton.dataset.turnable = 'true';
-          tempLastClickedButton.style.backgroundColor = 'white';
+          tempLastClickedButton.dataset.turnable = "true";
+          tempLastClickedButton.style.backgroundColor = "white";
           tempLastClickedButton.innerHTML = getgImage(0);
 
           lastKnownButtonId = undefined;
           lastKnownButtonNumber = undefined;
           wait = false;
         }, 1500);
-
       }
     }
-
   });
 }
 
-// showWinScreen();
-
-
 //functions
+
 function reset() {
   lastKnownButtonId = undefined;
   lastKnownButtonNumber = undefined;
@@ -89,86 +98,82 @@ function reset() {
   shuffle(numbers);
   distributeNumbers();
   matches = 0;
-
+  
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].innerHTML = getgImage(0);
-    buttons[i].style.backgroundColor = 'white';
+    buttons[i].style.backgroundColor = "white";
 
-    document.querySelector('.win-container').style.display = 'none';
+    document.querySelector(".win-container").style.display = "none";
 
-    document.getElementById("6").style.display = 'block';
-    document.getElementById("7").style.display = 'block';
-    document.getElementById("10").style.display = 'block';
-    document.getElementById("11").style.display = 'block';
-
+    document.getElementById("6").style.display = "block";
+    document.getElementById("7").style.display = "block";
+    document.getElementById("10").style.display = "block";
+    document.getElementById("11").style.display = "block";
   }
 }
 
+// showWinScreen();
 
 function showWinScreen() {
-  document.querySelector('.win-container').style.display = 'flex';
+  document.querySelector(".win-container").style.display = "flex";
 
-  document.getElementById("6").style.display = 'none';
-  document.getElementById("7").style.display = 'none';
-  document.getElementById("10").style.display = 'none';
-  document.getElementById("11").style.display = 'none';
-
-
+  document.getElementById("6").style.display = "none";
+  document.getElementById("7").style.display = "none";
+  document.getElementById("10").style.display = "none";
+  document.getElementById("11").style.display = "none";
 }
 
 function getgImage(number) {
   switch (number) {
-    case '0':
+    case "0":
       return '<img src="assets/images/card_0.jpg">';
-    case '1':
+    case "1":
       return '<img src="assets/images/card_1.jpg">';
-    case '2':
+    case "2":
       return '<img src="assets/images/card_2.jpg">';
-    case '3':
+    case "3":
       return '<img src="assets/images/card_3.jpg">';
-    case '4':
+    case "4":
       return '<img src="assets/images/card_4.jpg">';
-    case '5':
+    case "5":
       return '<img src="assets/images/card_5.jpg">';
-    case '6':
+    case "6":
       return '<img src="assets/images/card_6.jpg">';
-    case '7':
+    case "7":
       return '<img src="assets/images/card_7.jpg">';
-    case '8':
+    case "8":
       return '<img src="assets/images/card_8.jpg">';
-    case '9':
+    case "9":
       return '<img src="assets/images/card_9.jpg">';
     default:
       return '<img src="assets/images/card_back.jpg">';
-
   }
 }
 
 function getSound(number) {
   switch (number) {
-    case '0':
-      return 'playAudio(\'assets/sounds/0_number_morse_code.mp3\')';
-    case '1':
-      return 'playAudio(\'assets/sounds/1_number_morse_code.mp3\')';
-    case '2':
-      return 'playAudio(\'assets/sounds/2_number_morse_code.mp3\')';
-    case '3':
-      return 'playAudio(\'assets/sounds/3_number_morse_code.mp3\')';
-    case '4':
-      return 'playAudio(\'assets/sounds/4_number_morse_code.mp3\')';
-    case '5':
-      return 'playAudio(\'assets/sounds/5_number_morse_code.mp3\')';
-    case '6':
-      return 'playAudio(\'assets/sounds/6_number_morse_code.mp3\')';
-    case '7':
-      return 'playAudio(\'assets/sounds/7_number_morse_code.mp3\')';
-    case '8':
-      return 'playAudio(\'assets/sounds/8_number_morse_code.mp3\')';
-    case '9':
-      return 'playAudio(\'assets/sounds/9_number_morse_code.mp3\')';
+    case "0":
+      return "playAudio('assets/sounds/0_number_morse_code.mp3')";
+    case "1":
+      return "playAudio('assets/sounds/1_number_morse_code.mp3')";
+    case "2":
+      return "playAudio('assets/sounds/2_number_morse_code.mp3')";
+    case "3":
+      return "playAudio('assets/sounds/3_number_morse_code.mp3')";
+    case "4":
+      return "playAudio('assets/sounds/4_number_morse_code.mp3')";
+    case "5":
+      return "playAudio('assets/sounds/5_number_morse_code.mp3')";
+    case "6":
+      return "playAudio('assets/sounds/6_number_morse_code.mp3')";
+    case "7":
+      return "playAudio('assets/sounds/7_number_morse_code.mp3')";
+    case "8":
+      return "playAudio('assets/sounds/8_number_morse_code.mp3')";
+    case "9":
+      return "playAudio('assets/sounds/9_number_morse_code.mp3')";
     default:
-      return ' ';
-
+      return " ";
   }
 }
 
@@ -179,29 +184,31 @@ function playAudio(url) {
 function distributeNumbers() {
   for (i = 0; i < buttons.length; i++) {
     buttons[i].dataset.number = numbers[i];
-
+    buttons[i].dataset.turnable = "true";
+    buttons[i].setAttribute("onclick", getSound(buttons[i].dataset.number)); //getgsound(0)
   }
 }
 
 //get 8 random and double selected number in new array
 
 function getRandom(arr, n) {
-  var m = n*2;
+  var m = n * 2;
   var result = new Array(n),
-      len = arr.length,
-      taken = new Array(len);
+    len = arr.length,
+    taken = new Array(len);
   while (n--) {
-      var x = Math.floor(Math.random() * len);
-      result[n] = arr[x in taken ? taken[x] : x];
-      taken[x] = --len in taken ? taken[len] : len;
+    var x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
   }
-  var i,j = 0;
+  var i,
+    j = 0;
   var resultDouble = new Array(m);
   for (i = 0; i < result.length; i++) {
-      resultDouble[j]=result[i];
-      resultDouble[j+1]=result[i];
-      j = j + 2;
-}
+    resultDouble[j] = result[i];
+    resultDouble[j + 1] = result[i];
+    j = j + 2;
+  }
   return resultDouble;
 }
 
