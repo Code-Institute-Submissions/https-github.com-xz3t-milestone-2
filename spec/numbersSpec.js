1d7cf2ca60b2fe650d9a54c3b614051e3669f01a
xz3t <leu.valentin@gmail.com>
2020-09-01 21:12:50 +0000

    jasmin test a switch function fail return just default value

--- spec/numbersSpec.js
+++ spec/numbersSpec.js
@@ -1,23 +1,17 @@
-describe("getSound", function() {
-    // A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function.
-    
-    beforeEach(function() {
-        getSound = new getSound();
-    });
-    
-    describe("Should return asigned string for each number", function() {
-        // Specs are defined by calling the global Jasmine function it
-        it("should exist", function() {
-            // Expectations are built with the function expect which takes a value, called the actual. 
-            // Each matcher implements a boolean comparison between the actual value and the expected value.
-            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
-            expect(getSound).toBeDefined(number);
-        });
-        
-        it("should return playAudio('assets/sounds/0_number_morse_code.mp3') when called as getSound(0)", function() {
-            var result = getSound(0)
-            expect(result).toBe("playAudio('assets/sounds/0_number_morse_code.mp3')");
-        });
+describe("getgImage", function () {
 
+  beforeEach(function () {
+    image = new getgImage();
+  });
+
+  describe("Should return asigned string for each number", function () {
+    it("should exist", function () {
+      expect(getgImage).toBeDefined();
     });
+
+    it("retuns just default value ??", function () {
+      result = getgImage();
+      expect(result).toBe("<img src=\"assets/images/card_back.jpg\">");
+    });
+  });
 });
