ae144ff519ca2fafde783780affc1114e7f3f822
xz3t <leu.valentin@gmail.com>
2020-10-06 15:54:02 +0000

    Fix warning from JSHint validator reffering to  Unreachable 'break' after 'return'

--- assets/js/card-game.js
+++ assets/js/card-game.js
@@ -204,40 +204,30 @@ function showWinScreen() {
   document.getElementById("11").style.display = "none";
 }
 
-//will return a string for each number from array with acording image for Letters game
+//will return a string for each number from array with acording image for Numbers game
 
 function getgImage(number) {
   switch (number) {
     case "0":
       return '<img src="assets/images/card_0.jpg">';
-      break;
     case "1":
       return '<img src="assets/images/card_1.jpg">';
-      break;
     case "2":
       return '<img src="assets/images/card_2.jpg">';
-      break;
     case "3":
       return '<img src="assets/images/card_3.jpg">';
-      break;
     case "4":
       return '<img src="assets/images/card_4.jpg">';
-      break;
     case "5":
       return '<img src="assets/images/card_5.jpg">';
-      break;
     case "6":
       return '<img src="assets/images/card_6.jpg">';
-      break;
     case "7":
       return '<img src="assets/images/card_7.jpg">';
-      break;
     case "8":
       return '<img src="assets/images/card_8.jpg">';
-      break;
     case "9":
       return '<img src="assets/images/card_9.jpg">';
-      break;
     default:
       return '<img src="assets/images/card_back.jpg">';
   }
@@ -249,34 +239,24 @@ function getSound(number) {
   switch (number) {
     case "0":
       return "playAudio('assets/sounds/0_number_morse_code.mp3')";
-      break;
     case "1":
       return "playAudio('assets/sounds/1_number_morse_code.mp3')";
-      break;
     case "2":
       return "playAudio('assets/sounds/2_number_morse_code.mp3')";
-      break;
     case "3":
       return "playAudio('assets/sounds/3_number_morse_code.mp3')";
-      break;
     case "4":
       return "playAudio('assets/sounds/4_number_morse_code.mp3')";
-      break;
     case "5":
       return "playAudio('assets/sounds/5_number_morse_code.mp3')";
-      break;
     case "6":
       return "playAudio('assets/sounds/6_number_morse_code.mp3')";
-      break;
     case "7":
       return "playAudio('assets/sounds/7_number_morse_code.mp3')";
-      break;
     case "8":
       return "playAudio('assets/sounds/8_number_morse_code.mp3')";
-      break;
     case "9":
       return "playAudio('assets/sounds/9_number_morse_code.mp3')";
-      break;
     default:
       return " ";
   }
@@ -288,82 +268,56 @@ function getgLetterImage(number) {
   switch (number) {
     case "0":
       return '<img src="assets/images/card_A.jpg">';
-      break;
     case "1":
       return '<img src="assets/images/card_B.jpg">';
-      break;
     case "2":
       return '<img src="assets/images/card_C.jpg">';
-      break;
     case "3":
       return '<img src="assets/images/card_D.jpg">';
-      break;
     case "4":
       return '<img src="assets/images/card_E.jpg">';
-      break;
     case "5":
       return '<img src="assets/images/card_F.jpg">';
-      break;
     case "6":
       return '<img src="assets/images/card_G.jpg">';
-      break;
     case "7":
       return '<img src="assets/images/card_H.jpg">';
-      break;
     case "8":
       return '<img src="assets/images/card_I.jpg">';
-      break;
     case "9":
       return '<img src="assets/images/card_J.jpg">';
-      break;
     case "10":
       return '<img src="assets/images/card_K.jpg">';
-      break;
     case "11":
       return '<img src="assets/images/card_L.jpg">';
-      break;
     case "12":
       return '<img src="assets/images/card_M.jpg">';
-      break;
     case "13":
       return '<img src="assets/images/card_N.jpg">';
-      break;
     case "14":
       return '<img src="assets/images/card_O.jpg">';
-      break;
     case "15":
       return '<img src="assets/images/card_P.jpg">';
-      break;
     case "16":
       return '<img src="assets/images/card_Q.jpg">';
-      break;
     case "17":
       return '<img src="assets/images/card_R.jpg">';
-      break;
     case "18":
       return '<img src="assets/images/card_S.jpg">';
-      break;
     case "19":
       return '<img src="assets/images/card_T.jpg">';
-      break;
     case "20":
       return '<img src="assets/images/card_U.jpg">';
-      break;
     case "21":
       return '<img src="assets/images/card_V.jpg">';
-      break;
     case "22":
       return '<img src="assets/images/card_W.jpg">';
-      break;
     case "23":
       return '<img src="assets/images/card_X.jpg">';
-      break;
     case "24":
       return '<img src="assets/images/card_Y.jpg">';
-      break;
     case "25":
       return '<img src="assets/images/card_Z.jpg">';
-      break;
     default:
       return '<img src="assets/images/card_back.jpg">';
   }
@@ -375,82 +329,56 @@ function getLettersSound(number) {
   switch (number) {
     case "0":
       return "playAudio('assets/sounds/A_morse_code.mp3')";
-      break;
     case "1":
       return "playAudio('assets/sounds/B_morse_code.mp3')";
-      break;
     case "2":
       return "playAudio('assets/sounds/C_morse_code.mp3')";
-      break;
     case "3":
       return "playAudio('assets/sounds/D_morse_code.mp3')";
-      break;
     case "4":
       return "playAudio('assets/sounds/E_morse_code.mp3')";
-      break;
     case "5":
       return "playAudio('assets/sounds/F_morse_code.mp3')";
-      break;
     case "6":
       return "playAudio('assets/sounds/G_morse_code.mp3')";
-      break;
     case "7":
       return "playAudio('assets/sounds/H_morse_code.mp3')";
-      break;
     case "8":
       return "playAudio('assets/sounds/I_morse_code.mp3')";
-      break;
     case "9":
       return "playAudio('assets/sounds/J_morse_code.mp3')";
-      break;
     case "10":
       return "playAudio('assets/sounds/K_morse_code.mp3')";
-      break;
     case "11":
       return "playAudio('assets/sounds/L_morse_code.mp3')";
-      break;
     case "12":
       return "playAudio('assets/sounds/M_morse_code.mp3')";
-      break;
     case "13":
       return "playAudio('assets/sounds/N_morse_code.mp3')";
-      break;
     case "14":
       return "playAudio('assets/sounds/O_morse_code.mp3')";
-      break;
     case "15":
       return "playAudio('assets/sounds/P_morse_code.mp3')";
-      break;
     case "16":
       return "playAudio('assets/sounds/Q_morse_code.mp3')";
-      break;
     case "17":
       return "playAudio('assets/sounds/R_morse_code.mp3')";
-      break;
     case "18":
       return "playAudio('assets/sounds/S_morse_code.mp3')";
-      break;
     case "19":
       return "playAudio('assets/sounds/T_morse_code.mp3')";
-      break;
     case "20":
       return "playAudio('assets/sounds/U_morse_code.mp3')";
-      break;
     case "21":
       return "playAudio('assets/sounds/V_morse_code.mp3')";
-      break;
     case "22":
       return "playAudio('assets/sounds/W_morse_code.mp3')";
-      break;
     case "23":
       return "playAudio('assets/sounds/X_morse_code.mp3')";
-      break;
     case "24":
       return "playAudio('assets/sounds/Y_morse_code.mp3')";
-      break;
     case "25":
       return "playAudio('assets/sounds/Z_morse_code.mp3')";
-      break;
     default:
       return " ";
   }
@@ -462,55 +390,38 @@ function getgPunctuationImage(number) {
   switch (number) {
     case "0":
       return '<img src="assets/images/card_Ampresand.jpg">';
-      break;
     case "1":
       return '<img src="assets/images/card_Apostrope.jpg">';
-      break;
     case "2":
       return '<img src="assets/images/card_At.jpg">';
-      break;
     case "3":
       return '<img src="assets/images/card_Colon.jpg">';
-      break;
     case "4":
       return '<img src="assets/images/card_Comma.jpg">';
-      break;
     case "5":
       return '<img src="assets/images/card_Dollar.jpg">';
-      break;
     case "6":
       return '<img src="assets/images/card_Equals.jpg">';
-      break;
     case "7":
       return '<img src="assets/images/card_Exclamation.jpg">';
-      break;
     case "8":
       return '<img src="assets/images/card_Hyphen.jpg">';
-      break;
     case "9":
       return '<img src="assets/images/card_ParanthesisClose.jpg">';
-      break;
     case "10":
       return '<img src="assets/images/card_ParanthesisOpen.jpg">';
-      break;
     case "11":
       return '<img src="assets/images/card_Period.jpg">';
-      break;
     case "12":
       return '<img src="assets/images/card_Plus.jpg">';
-      break;
     case "13":
       return '<img src="assets/images/card_Question.jpg">';
-      break;
     case "14":
       return '<img src="assets/images/card_Quotation.jpg">';
-      break;
     case "15":
       return '<img src="assets/images/card_Slash.jpg">';
-      break;
     case "16":
       return '<img src="assets/images/card_Underscore.jpg">';
-      break;
     default:
       return '<img src="assets/images/card_back.jpg">';
   }
@@ -522,55 +433,38 @@ function getPunctuationSound(number) {
   switch (number) {
     case "0":
       return "playAudio('assets/sounds/Morse_Code_-_Ampersand.mp3')";
-      break;
     case "1":
       return "playAudio('assets/sounds/Morse_Code_-_Apostrope.mp3')";
-      break;
     case "2":
       return "playAudio('assets/sounds/Morse_Code_-_At_(Commercial_At).mp3')";
-      break;
     case "3":
       return "playAudio('assets/sounds/Morse_Code_-_Colon.mp3')";
-      break;
     case "4":
       return "playAudio('assets/sounds/Morse_Code_-_Comma.mp3')";
-      break;
     case "5":
       return "playAudio('assets/sounds/Morse_Code_-_Dollar_Sign.mp3')";
-      break;
     case "6":
       return "playAudio('assets/sounds/Morse_Code_-_Equals.mp3')";
-      break;
     case "7":
       return "playAudio('assets/sounds/Morse_Code_-_Exclamation_Point.mp3')";
-      break;
     case "8":
       return "playAudio('assets/sounds/Morse_Code_-_Hyphen,_Minus.mp3')";
-      break;
     case "9":
       return "playAudio('assets/sounds/Morse_Code_-_Parenthesis_(Close).mp3')";
-      break;
     case "10":
       return "playAudio('assets/sounds/Morse_Code_-_Parenthesis_(Open).mp3')";
-      break;
     case "11":
       return "playAudio('assets/sounds/Morse_Code_-_Period.mp3')";
-      break;
     case "12":
       return "playAudio('assets/sounds/Morse_Code_-_Plus.mp3')";
-      break;
     case "13":
       return "playAudio('assets/sounds/Morse_Code_-_Question_Mark.mp3')";
-      break;
     case "14":
       return "playAudio('assets/sounds/Morse_Code_-_Quotation_Mark.mp3')";
-      break;
     case "15":
       return "playAudio('assets/sounds/Morse_Code_-_Slash.mp3')";
-      break;
     case "16":
       return "playAudio('assets/sounds/Morse_Code_-_Underscore.mp3')";
-      break;
     default:
       return " ";
   }
@@ -600,7 +494,7 @@ function playAudio(url) {
   if (playSound == true) {
     delaySound(url);
   } else {
-    playSound == false;
+    playSound = false;
   }
 }
 
